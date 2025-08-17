ce264d7af8268dc39091a4972534819376dc5598
Ankit Singh <codeSavvy.learn@gmail.com>
2025-08-18 01:10:01 +0530

    Update ADLS_mount.py

--- notebook/set up/ADLS_mount.py
+++ notebook/set up/ADLS_mount.py
@@ -2,7 +2,7 @@
 dbutils.fs.mount(
 source = "wasbs://bronze@adlsdevrcm.blob.core.windows.net",
 mount_point = "/mnt/bronze",
-extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'iISOM9JZ9spGd7H8yKqhyXsx8+E3Twcjx57qGsxMutD7x1ZrcpnEobZPvpGE+uR+6y4WTHzmFk9F+AStPbCC1A=='}
+extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'Account Key of storage contianer'}
 )
 
 # COMMAND ----------
@@ -19,7 +19,7 @@ dbutils.fs.ls("/mnt/landing")
 dbutils.fs.mount(
 source = "wasbs://silver@adlsdevrcm.blob.core.windows.net",
 mount_point = "/mnt/silver",
-extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'iISOM9JZ9spGd7H8yKqhyXsx8+E3Twcjx57qGsxMutD7x1ZrcpnEobZPvpGE+uR+6y4WTHzmFk9F+AStPbCC1A=='})
+extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'Account Key of storage contianer'})
 
 # COMMAND ----------
 
@@ -27,7 +27,7 @@ extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'iISOM9
 dbutils.fs.mount(
 source = "wasbs://gold@adlsdevrcm.blob.core.windows.net",
 mount_point = "/mnt/gold",
-extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'iISOM9JZ9spGd7H8yKqhyXsx8+E3Twcjx57qGsxMutD7x1ZrcpnEobZPvpGE+uR+6y4WTHzmFk9F+AStPbCC1A=='})
+extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'Account Key of storage contianer'})
 
 # COMMAND ----------
 
@@ -35,4 +35,4 @@ extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'iISOM9
 dbutils.fs.mount(
 source = "wasbs://landing@adlsdevrcm.blob.core.windows.net",
 mount_point = "/mnt/landing",
-extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'iISOM9JZ9spGd7H8yKqhyXsx8+E3Twcjx57qGsxMutD7x1ZrcpnEobZPvpGE+uR+6y4WTHzmFk9F+AStPbCC1A=='})
+extra_configs = {"fs.azure.account.key.adlsdevrcm.blob.core.windows.net":'Account Key of storage contianer'})
